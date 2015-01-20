67322882cd167908cb1ed5e9a062cdc5881e8ac7
Jeff Byrnes <jeff@evertrue.com>
2015-01-20 11:01:54 -0500

    Add Capistrano-based deployment

---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+set :application, 'byrnesgrayson.com'
+set :repo_url, 'git@bitbucket.org:jeffbyrnes/byrnesgrayson.com.git'
+set :scm, :git
+
+server 'byrnesgrayson.com', user: 'deploy', roles: %w(web app)
+
+# Default value for :log_level is :debug
+set :log_level, :debug
+
+before 'deploy:published', 'gulp'
