3e01f98f98c71fc498d197cc2aac54a8c924e33a
Jeff Byrnes <jeff@evertrue.com>
2015-01-23 15:13:42 -0500

    Ensure homepage .copy renders properly

--- _home.php
+++ _home.php
@@ -1,5 +1,5 @@
 <img class="main-photo" src="/img/fancy-couple.jpg" alt="Morgan and Ryan in formal attire">
 
-<section class="essentials">
+<section class="copy">
     <img src="/img/essentials.png" alt="Save the Date, Morgan+Ryan, 9.19.2015, Palmetto Bluff, SC">
 </section>
