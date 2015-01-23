4fba81b0d537234ab8ff859652356f3d89de1b45
Jeff Byrnes <jeff@evertrue.com>
2015-01-23 17:25:36 -0500

    Add class name to homepage “text”

--- _home.php
+++ _home.php
@@ -1,5 +1,5 @@
 <img class="main-photo" src="/img/fancy-couple.jpg" alt="Morgan and Ryan in formal attire">
 
 <section class="copy">
-    <img src="/img/essentials.png" alt="Save the Date, Morgan+Ryan, 9.19.2015, Palmetto Bluff, SC">
+    <img class="essentials" src="/img/essentials.png" alt="Save the Date, Morgan+Ryan, 9.19.2015, Palmetto Bluff, SC">
 </section>
