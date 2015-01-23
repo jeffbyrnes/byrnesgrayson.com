dc94e25c1ce5ffd831010e973826c804bab0ee17
Jeff Byrnes <jeff@evertrue.com>
2015-01-23 17:24:31 -0500

    Move navigation outside .container

--- index.php
+++ index.php
@@ -13,31 +13,31 @@ if (in_array($_SERVER['REQUEST_URI'], array('/', '/index.php'))) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body class="<?php echo $page; ?>">
+    <header class="centered-navigation">
+        <div class="centered-navigation-wrapper">
+            <a href="/" class="mobile-logo">
+                <img src="/img/monogram.png" alt="GMR monogram">
+            </a>
+
+            <a href="javascript:void(0)" id="js-centered-navigation-mobile-menu" class="centered-navigation-mobile-menu">MENU</a>
+
+            <ul id="js-centered-navigation-menu" class="centered-navigation-menu show">
+                <li class="nav-link"><a href="/">Welcome</a></li>
+                <li class="nav-link nav-separator">•</li>
+                <li class="nav-link"><a href="/when-where">When+Where</a></li>
+                <li class="nav-link logo">
+                    <a href="/">
+                        <img src="/img/monogram.png" alt="GMR monogram">
+                    </a>
+                </li>
+                <li class="nav-link"><a href="/stay-play">Stay+Play</a></li>
+                <li class="nav-link nav-separator">•</li>
+                <li class="nav-link"><a href="/bridal-party">Bridal Party</a></li>
+            </ul>
+        </div>
+    </header>
+
     <div class="container">
-        <header class="centered-navigation">
-            <div class="centered-navigation-wrapper">
-                <a href="/" class="mobile-logo">
-                    <img src="/img/monogram.png" alt="GMR monogram">
-                </a>
-
-                <a href="javascript:void(0)" id="js-centered-navigation-mobile-menu" class="centered-navigation-mobile-menu">MENU</a>
-
-                <ul id="js-centered-navigation-menu" class="centered-navigation-menu show">
-                    <li class="nav-link"><a href="/">Welcome</a></li>
-                    <li class="nav-link nav-separator">•</li>
-                    <li class="nav-link"><a href="/when-where">When+Where</a></li>
-                    <li class="nav-link logo">
-                        <a href="/">
-                            <img src="/img/monogram.png" alt="GMR monogram">
-                        </a>
-                    </li>
-                    <li class="nav-link"><a href="/stay-play">Stay+Play</a></li>
-                    <li class="nav-link nav-separator">•</li>
-                    <li class="nav-link"><a href="/bridal-party">Bridal Party</a></li>
-                </ul>
-            </div>
-        </header>
-
         <div class="content">
             <?php
             require_once("_{$page}.php");
