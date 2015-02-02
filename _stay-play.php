49e59355234efba35bd8e099a20675ca07a2514c
Jeff Byrnes <jeff@evertrue.com>
2015-02-02 12:29:49 -0500

    Update Stay+Play copy as per 1/31 email from Morgs

--- _stay-play.php
+++ _stay-play.php
@@ -9,9 +9,20 @@
 
     <p>Palmetto Bluff is also only 15 minutes off of Highway 95.</p>
 
+    <p>There are transportation services you can make a reservation with to take you to/from the Savannah airport to the resort or your nearby hotel:</p>
+
+    <ul class="list">
+        <li><a href="http://palmettotransportation.com/">Palmetto Transportation</a></li>
+        <li><a href="http://savannahairport.com/transportation">Savannah Airport Transportation</a></li>
+        <li><a href="http://www.savannahairportshuttle.com">Savannah Airport Shuttle & Limousine</a></li>
+        <li><a href="http://hiltonheadrides.com">Diamond Transportation</a></li>
+    </ul>
+
     <p>There are cottages and houses on the resort premises, which you can rent for the weekend. Cottages are Palmetto Bluff’s version of a hotel room with 1 bedroom, bathroom and screened porch overlooking the May River or lagoon while the houses are 2+bedrooms and have the full amenities of a house.</p>
 
-    <p>We have room blocks at both the <a href="http://hamptoninn3.hilton.com/en/hotels/south-carolina/hampton-inn-and-suites-bluffton-sun-city-BFNSCHX/index.html">Hampton Inn</a> and <a href="http://www.ihg.com/holidayinnexpress/hotels/us/en/bluffton/btosc/hoteldetail">Holiday Inn</a> and we will be providing trolley transportation to and from on Saturday for the wedding festivities. Room blocks are under <em>Byrnes Grayson Wedding</em>.</p>
+    <p>To make your reservation at the resort, please call reservations at Palmetto Bluff and mention you are reserving under the Byrnes Grayson wedding cottage block.</p>
+
+    <p>Additionally, we have room blocks at both the <a href="http://hamptoninn3.hilton.com/en/hotels/south-carolina/hampton-inn-and-suites-bluffton-sun-city-BFNSCHX/index.html">Hampton Inn</a> and <a href="http://www.ihg.com/holidayinnexpress/hotels/us/en/bluffton/btosc/hoteldetail">Holiday Inn</a>, and we will be providing trolley transportation to and from on Saturday for the wedding festivities. Room blocks are under <em>Byrnes Grayson Wedding</em>.</p>
 
     <p>While you’re in town for our wedding, enjoy yourself — make a tee time, go fishing, get a spa treatment, lay by the pool, or take a boat ride over the sandbar!</p>
 </section>
