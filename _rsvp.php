defd3bdd5a6296c6ced6e55a8b6db53df86e944a
Jeff Byrnes <jeff@evertrue.com>
2015-02-09 16:28:05 -0500

    Add Google Forms-powered RSVP

--- _rsvp.php
+++ _rsvp.php
@@ -2,8 +2,67 @@
 
 </div>
 
-<section class="copy">
+<section class="copy full-width">
     <h1 class="page-title">RSVP</h1>
 
-    <p>We’ll have a form here, soon enough</p>
+    <p class="required-asterisk">* Required</p>
+
+    <form class="rsvp-form" method="post" action="https://docs.google.com/forms/d/14H6BBjBK272VfGgKqNq6h5fm0wCoC8P6HGFTXhkxIlQ/formResponse?embedded=true" data-abide>
+        <ol>
+            <li class="text-horiz">
+                <label class="item-label" for="entry_1861052402">
+                    <p class="title">
+                        First Name <span class="required-asterisk">*</span>
+                    </p>
+
+                    <input type="text" name="entry.1861052402" id="entry_1861052402" required>
+                </label>
+            </li>
+
+            <li class="text-horiz last">
+                <label class="item-label" for="entry_1769345773">
+                    <p class="title">
+                        Last Name <span class="required-asterisk">*</span>
+                    </p>
+
+                    <input type="text" name="entry.1769345773" id="entry_1769345773" required>
+                </label>
+            </li>
+
+            <li>
+                <label class="item-label" for="entry_1326483091">
+                    <p class="title">
+                        Attending? <span class="required-asterisk">*</span>
+                    </p>
+                </label>
+
+                <ul class="choices">
+                    <li class="choice-item">
+                        <label>
+                            <input type="radio" name="entry.630043729" value="Wouldn&rsquo;t miss it!" id="group_630043729_1" class="radio" required>
+                            <span class="choice-label">Wouldn’t miss it!</span>
+                        </label>
+                    </li>
+
+                    <li class="choice-item">
+                        <label>
+                            <input type="radio" name="entry.630043729" value="Deepest regrets" id="group_630043729_2" class="radio" required>
+                            <span class="choice-label">Deepest regrets</span>
+                        </label>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <input type="submit" name="submit" value="Submit" id="submit">
+            </li>
+        </ol>
+
+        <input type="hidden" name="draftResponse" value="[,,&quot;-1069075591387856625&quot;]
+        ">
+        <input type="hidden" name="pageHistory" value="0">
+        <input type="hidden" name="fbzx" value="-1069075591387856625">
+    </form>
+
+    <!-- <script src="https://docs.google.com/static/forms/client/js/366246113-formviewer_prd.js"></script> -->
 </section>
