9c461eb6fc888ee332bf278d0f9f557300be1087
Jeff Byrnes <jeff@evertrue.com>
2015-02-16 17:56:51 -0500

    Update Stay+Play copy as per final revisions

--- _stay-play.php
+++ _stay-play.php
@@ -6,11 +6,9 @@
 <section class="copy">
     <h1 class="page-title">Stay + Play</h1>
 
-    <p>We recommend flying into Savannah, GA, but Hilton Head, SC also has a small airport you can fly into. Both are approximately a 30 minute drive to Palmetto Bluff.</p>
+    <p>We recommend flying into Savannah, GA or Hilton Head, SC. Both are approximately a 30 minute drive to Palmetto Bluff.</p>
 
-    <p>Palmetto Bluff is also only 15 minutes off of Highway 95.</p>
-
-    <p>There are transportation services you can make a reservation with to take you to/from the Savannah airport to the resort or your nearby hotel:</p>
+    <p>Here are a couple local car/shuttle services that offer transport from the Savannah airport to Palmetto Bluff:</p>
 
     <ul class="list">
         <li><a href="http://palmettotransportation.com/">Palmetto Transportation</a></li>
@@ -19,13 +17,15 @@
         <li><a href="http://hiltonheadrides.com">Diamond Transportation</a></li>
     </ul>
 
-    <p>There are cottages and houses on the resort premises, which you can rent for the weekend. Cottages are Palmetto Bluff’s version of a hotel room with 1 bedroom, bathroom and screened porch overlooking the May River or lagoon while the houses are 2+bedrooms and have the full amenities of a house.</p>
+    <p>There are three places we have reserved for our guests to stay during our wedding weekend:</p>
 
-    <p>To make your reservation at the resort, please call reservations at Palmetto Bluff and mention you are reserving under the Byrnes Grayson wedding cottage block.</p>
+    <ul class="list">
+        <li><a href="http://www.palmettobluff.com">Palmetto Bluff Resort</a> – call the inn and mention Byrnes Grayson Wedding</li>
+        <li><a href="http://hamptoninn3.hilton.com/en/hotels/south-carolina/hampton-inn-and-suites-bluffton-sun-city-BFNSCHX/index.html">Hampton Inn</a> – call and mention Byrnes Grayson Wedding</li>
+        <li><a href="http://www.hiexpress.com/redirect?path=hd&brandCode=ex&localeCode=en&regionCode=1&hotelCode=BTOSC&_PMID=99801505&GPC=BGW">Holiday Inn</a> – call and mention Byrnes Grayson Wedding, or book online with block code <abbr>BGW</abbr></li>
+    </ul>
 
-    <p>Additionally, we have room blocks at both the <a href="http://hamptoninn3.hilton.com/en/hotels/south-carolina/hampton-inn-and-suites-bluffton-sun-city-BFNSCHX/index.html">Hampton Inn</a>, and <a href="http://www.hiexpress.com/redirect?path=hd&brandCode=ex&localeCode=en&regionCode=1&hotelCode=BTOSC&_PMID=99801505&GPC=BGW">Holiday Inn</a>. Room blocks are under <em>Byrnes Grayson Wedding</em>, or block code BGW.</p>
+    <p>We will be providing trolley transportation to and from the Hampton Inn and Holiday Inn on Saturday for the wedding festivities.</p>
 
-    <p>We will be providing trolley transportation to and from on Saturday for the wedding festivities.</p>
-
-    <p>While you’re in town for our wedding, enjoy yourself — make a tee time, go fishing, get a spa treatment, lay by the pool, or take a boat ride over the sandbar!</p>
+    <p>While you’re in town for our wedding, enjoy yourself — make a tee time, go fishing, get a spa treatment, lay by the pool, or take a boat ride over to the sandbar!</p>
 </section>
