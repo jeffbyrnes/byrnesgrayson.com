aa500110682a65b54f1065338672d45718102b0c
Jeff Byrnes <jeff@evertrue.com>
2015-04-07 15:39:53 -0400

    Some final copy tweaks

--- _when-where.php
+++ _when-where.php
@@ -11,26 +11,27 @@
     <h2><a href="http://www.palmettobluff.com/">Palmetto Bluff<br>Bluffton, South Carolina</a></h2>
 
     <dl>
-        <dt><time datetime="2015-09-18">FRIDAY</time></dt>
+        <dt><time datetime="2015-09-18">FRIDAY the 18th</time></dt>
         <dd>
             join us by the fire pits!<br>
             (smart casual attire)
         </dd>
 
-        <dt><time datetime="2015-09-19">SATURDAY</time></dt>
+        <dt><time datetime="2015-09-19">SATURDAY the 19th</time></dt>
         <dd>
             5p: ceremony at the Chapel<br>
-            5:30p–6:30p: waterside cocktails<br>
-            6:30–10p: dinner + dancing on the Village Green<br>
-            10p–12p: after party and dancing upstairs in the River House<br>
+            5:30p: waterside cocktails<br>
+            6:30p: dinner + dancing on the Village Green<br>
+            10p–12a: after party and dancing upstairs in the River House<br>
             (black tie optional attire)<br>
             * There will be trolley service to/from the Hampton and Holiday Inns
         </dd>
 
-        <dt><time datetime="2015-09-20">SUNDAY</time></dt>
+        <dt><time datetime="2015-09-20">SUNDAY the 20th</time></dt>
         <dd>
             10a: pop by the Byrnes house for a morning toast and brunch bites<br>
-            (casual attire)
+            (casual attire)<br>
+            * All events take place at Palmetto Bluff resort and are walkable
         </dd>
     </dl>
 </section>
